8c61122d3d0283996abd377cb2a674e51fdb9de4
Gorobey Artem <artemgo@123softawre.ru>
2018-07-10 10:19:35 +0300

    add pre-loader

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import Loader from '../Loader';
+import luke from '../../images/luke.jpg';
+import chewbacca from '../../images/chewbacca.jpg';
 
 const propTypes = {
     loading: PropTypes.bool,
@@ -9,35 +11,34 @@ const propTypes = {
 };
 
 const Character = ({ loading, error, hero }) => {
-    if (loading) {
-        return <Loader />;
-    }
-    if (error) {
-        return <h1>ERROR</h1>;
-    }
-    return (
+    if (loading) return <Loader />;
+    if (error) return <div><h1>ERROR</h1><img src={chewbacca} alt="error"/></div>;
+    const heroFriends = hero && hero.friends && (
+        hero.friends.map(
+            friend => friend && (
+                <h6 key={friend.id}>
+                    {friend.name}
+              :
+                    {friend.appearsIn.map(item => item && item.toLowerCase()).join(', ')}
+                </h6>
+            ),
+        )
+    );
+    const currentHero = hero && (
         <div>
-            {hero
-                    && (
-                        <div>
-                            <h3>
-                                {hero.name}
-                            </h3>
-                            {
-                                hero.friends
-                                    && hero.friends.map(
-                                        friend => friend && (
-                                            <h6 key={friend.id}>
-                                                {friend.name}
-:
-                                                {friend.appearsIn.map(item => item && item.toLowerCase()).join(', ')}
-                                            </h6>
-                                        ),
-                                    )
-                            }
-                        </div>
-                    )
-            }
+            <h3>{hero.name}</h3>
+            <img
+                style={{
+                    width: '90px', height: '90px', objectFit: 'cover', borderRadius: '50%',
+                }}
+                src={luke}
+            />
+        </div>
+    );
+    return (
+        <div className="sw-character-wrap">
+            {currentHero}
+            {heroFriends}
         </div>
     );
 };
